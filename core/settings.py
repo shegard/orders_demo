a8c9250f2cf491d4df90db6f0ccea0233c18a763
Oleg <bublik_oleg@hotmail.com>
2019-03-04 03:04:03 +0200

    deployment preps

--- core/settings.py
+++ core/settings.py
@@ -78,18 +78,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'orders_demo_db',
-    #     'USER': 'orders_demo_usr',
-    #     'PASSWORD': 'MzEmcnV6',
-    #     'HOST': 'localhost',
-    #     'PORT': 5432
-    # }
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'orders_demo_db',
+        'USER': 'orders_demo_usr',
+        'PASSWORD': 'MzEmcnV6',
+        'HOST': 'localhost',
+        'PORT': 5432
+    }
 }
 """
 CREATE DATABASE orders_demo_db;
